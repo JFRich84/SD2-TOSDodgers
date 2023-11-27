5d2c107e1d3480f50a706678114736df47919201
JFRich84 <71397450+JFRich84@users.noreply.github.com>
2023-11-27 12:06:15 +0000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -7,18 +7,59 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    // Set up an array of data
+    var test_data = ['one', 'two', 'three', 'four'];
+    // Send the array through to the template as a variable called data
+    res.render("index", {'title':'My index page', 'heading':'My heading', 'data':test_data});
+});
+
+// Task 1 JSON formatted listing of students 
+app.get("/all-students", function(req, res) {
+    var sql = 'select * from Students';
+
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results);
+    });
+});
+
+
+// Task 2 display a formatted list of students
+app.get("/all-students-formatted", function(req, res) {
+    var sql = 'select * from Students';
+    db.query(sql).then(results => {
+    	    // Send the results rows to the all-students template
+    	    // The rows will be in a variable called data
+        res.render('all-students', {data: results});
+    });
+});
+
+// task 3 single student page
+app.get("single-student/:id", function (req, res){
+    var stId = req.params.id;
+    console.log(stId);
+    var st5ql = "SELECT s.name as student, ps.name as programme, \
+    ps.id as pcode from Students s \
+    JOIN Student_Programme sp on sp.id = s.id \
+    JOIN Programmes ps on ps.id = sp.programme \
+    WHERE s.id = ?";
+    var modsql = "SELECT * FROM Programme_Modules pm \
+    JOIN Modules m on m.code = pm.module"
 });
 
 // Create a route for testing the db
 app.get("/db_test", function(req, res) {
     // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
+    sql = 'select * from Students';
     db.query(sql).then(results => {
         console.log(results);
         res.send(results)
